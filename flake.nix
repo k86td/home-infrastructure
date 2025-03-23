d5a2dd49f74a541104d36c23c58dba42e2c1eb45
Tristan Lepine <tristanlepine14@gmail.com>
2025-03-23 14:51:04 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake containing required tools to deploy infra";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        ansible
+      ];
+    };
+  };
+
+}
