e0361d3dc59366afc4e8d901a47cde4e4f2cffd2
Tristan Lepine <tristan.lepine@ebox.ca>
2025-03-31 16:15:23 -0400

    fix: updated some tasks to use kubernetes module

--- flake.nix
+++ flake.nix
@@ -11,10 +11,17 @@
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       buildInputs = with pkgs; [
-        ansible
+        (python3.withPackages (py-pkgs: [
+          py-pkgs.ansible-core
+          py-pkgs.kubernetes
+        ]))
       ];
 
       LC_ALL = "C.UTF-8";
+    
+      shellHook = ''
+        export KUBECONFIG="$(git rev-parse --show-toplevel)/ansible/artifacts/kubeconfig.yaml"
+      '';
     };
   };
 
